7e14a428844f062ab3ccab17dc61543bc7942228
Marco Lau <lau.career@gmail.com>
2013-10-08 22:01:54 +0800

    Build contact lists graphical layout

--- gen/com/example/addressbookapp/R.java
+++ gen/com/example/addressbookapp/R.java
@@ -25,7 +25,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080002;
+        public static final int contactsTitleTextView=0x7f080001;
+        public static final int list=0x7f080004;
+        public static final int tableRow1=0x7f080000;
+        public static final int tableRow2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,8 +40,19 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_button=0x7f050009;
+        public static final int add_contact=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int contacts_title=0x7f050008;
+        public static final int delete_button=0x7f05000d;
+        public static final int edit_button=0x7f05000c;
+        public static final int edit_contact_title=0x7f05000b;
+        public static final int email_address=0x7f050006;
+        public static final int first_name=0x7f050003;
+        public static final int home_address=0x7f050007;
+        public static final int last_name=0x7f050004;
+        public static final int phone_number=0x7f050005;
+        public static final int save_data=0x7f05000a;
     }
     public static final class style {
         /** 
