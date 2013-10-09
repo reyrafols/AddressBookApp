05649e4dbf6021b70ae6a0b3e4262d29e7dd9136
Marco Lau <lau.career@gmail.com>
2013-10-09 16:26:33 +0800

    Build Add Contact and Edit Contact graphical layout

--- gen/com/example/addressbookapp/R.java
+++ gen/com/example/addressbookapp/R.java
@@ -25,15 +25,40 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f08001b;
+        public static final int addButton=0x7f080015;
+        public static final int addContactTextView=0x7f080005;
+        public static final int addressTextView=0x7f080012;
         public static final int button1=0x7f080002;
+        public static final int contactId=0x7f080016;
+        public static final int contactName=0x7f080017;
         public static final int contactsTitleTextView=0x7f080001;
+        public static final int deleteButton=0x7f08001a;
+        public static final int editButton=0x7f080019;
+        public static final int editContactTextView=0x7f080018;
+        public static final int emailAddress=0x7f080010;
+        public static final int emailTextView=0x7f08000f;
+        public static final int firstName=0x7f080007;
+        public static final int firstNameTextView=0x7f080006;
+        public static final int homeAddress=0x7f080013;
+        public static final int lastName=0x7f08000a;
+        public static final int lastNameTextView=0x7f080009;
         public static final int list=0x7f080004;
+        public static final int phoneNumber=0x7f08000d;
+        public static final int phoneNumberTextView=0x7f08000c;
         public static final int tableRow1=0x7f080000;
         public static final int tableRow2=0x7f080003;
+        public static final int tableRow3=0x7f080008;
+        public static final int tableRow4=0x7f08000b;
+        public static final int tableRow5=0x7f08000e;
+        public static final int tableRow6=0x7f080011;
+        public static final int tableRow7=0x7f080014;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int add_new_contact=0x7f030001;
+        public static final int contact_entry=0x7f030002;
+        public static final int edit_contact=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
